f6e2daac8271a72952f8e4f496faea7afcd1b442
CHardy <christianrh2000@hotmail.com>
2022-11-16 09:14:55 -0300

    springPacientes proyect commit 1

---
+++ springPacientes/src/main/java/com/chardy/springPacientes/SpringPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.chardy.springPacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPacientesApplication.class, args);
+	}
+
+}
---
+++ springPacientes/src/test/java/com/chardy/springPacientes/SpringPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chardy.springPacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
